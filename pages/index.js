202962d0360ebea2ab41ee4e82f5e1b6fe37d646
David Portocarrero Caicedo <ing.portocarrero@gmail.com>
2019-03-11 00:16:28 -0500

    [update] styles an adding new components

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,26 @@
  * Landing page
  */
 import NavegadorGlobal from '../components/NavegadorGlobal'
+import Portada from '../components/Portada'
+import SeccionQueEs from '../components/SeccionQueEs'
+
+import Grid from '@material-ui/core/Grid';
+
 export default class extends React.Component {
     render(){
         return <div>
-            <NavegadorGlobal />
-            hola
-            {
-                /*
+            <Grid container spacing={0} >
+                <NavegadorGlobal />
                 <Portada />
                 <SeccionQueEs />
-                <SeccionComoFunciona />
-                <SeccionEquipo />
-                <SeccionContacto />
-                */
-            }
+                {
+                    /*
+                    <SeccionComoFunciona />
+                    <SeccionEquipo />
+                    <SeccionContacto />
+                    */
+                }
+            </Grid>
 
             <style jsx global>{`
                 
