226fd0facd97cb21a5b3641e591f224cf97d108a
ingporto <ing.portocarrero@gmail.com>
2019-03-27 20:30:58 -0500

    [update] adding the new component and body style

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,19 @@
  */
 import NavegadorGlobal from '../components/NavegadorGlobal'
 import Portada from '../components/Portada'
+import LaptopScreenshot from '../components/LaptopScreenshot'
 import SeccionQueEs from '../components/SeccionQueEs'
+
 
 import Grid from '@material-ui/core/Grid';
 
 export default class extends React.Component {
     render(){
-        return <div>
+        return <div className="mainContainer">
             <Grid container spacing={0} >
                 <NavegadorGlobal />
                 <Portada />
+                <LaptopScreenshot />
                 <SeccionQueEs />
                 {
                     /*
@@ -24,7 +27,14 @@ export default class extends React.Component {
             </Grid>
 
             <style jsx global>{`
-                
+                body {
+                    margin:0;
+                }
+                /*
+                .mainContainer{
+                    width: 1200px;
+                }
+                */
             `}</style>
         </div>
     }
