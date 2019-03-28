7991dc7998b4eb238df2f708918366334c5d0f6a
ingporto <ing.portocarrero@gmail.com>
2019-03-27 20:29:41 -0500

    [update] global menu to be like the design

--- components/Portada/index.js
+++ components/Portada/index.js
@@ -4,68 +4,76 @@
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 
 class main_Portada extends React.Component{
     render(){
         return (
-            <div className="Portada">
+            <div className="Portada" style={{
+                textAlign: 'center',
+            }}>
+                <div className="puzzleRight"></div>
+                <div className="puzzleLeft"></div>
                 <Grid item lg={12}>
-                    <Card className="card">
-                        <Grid container >
-                            <Grid item xs={6}>
-                                <div className="details">
-                                <Grid container >
-                                    <Grid item>
-                                        <CardContent className="content">
-                                            <Typography variant="body1">
-                                                Construye, diseña y despliega
-                                            </Typography>
-                                            <Typography variant="body1">
-                                                Servicios web en un lenguaje sencillo.
-                                            </Typography>
-                                        </CardContent >
-                                    </Grid>
-                                    <Grid item>
-                                        <div>
-                                            <Button 
-                                                className="accederBtn" 
-                                                variant="contained" 
-                                                color="primary" 
-                                                style={
-                                                    {
-                                                        color:'white', 
-                                                        background:'#2196f3'
-                                                    }
-                                                }>
-                                                Empieza gratis
-                                                <KeyboardArrowRight />
-                                            </Button>
-                                        </div>
-                                    </Grid>
-                                </Grid>
-                                </div>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <CardMedia 
-                                    className="cover"
-                                    image="/static/img_portada.jpg" 
-                                    title="Imagen aluciva al entorno de desarrollo" 
-                                    style={{
-                                        height: '150px'
-                                    }}
-                                />
-                            </Grid>
+                    <Grid container>
+                        <Grid item xs={6} style={{
+                            margin:'0 auto'
+                        }}>
+                            <Typography variant="h3" style={{
+                                color:'white',
+                                fontSize: '30pt',
+                                margin: '70px 0 18px 0'
+                            }}>
+                                Construye, diseña y despliega
+                            </Typography>
+                            <Typography variant="body1" style={{
+                                color:'white',
+                                marginBottom: '25px'
+                            }}>
+                                Servicios web en un lenguaje sencillo. No requieres aprender un nuevo lenguaje de programación, lo puedes lograr con tus habilidades lógicas.
+                            </Typography>
+                            <Button 
+                                className="accederBtn" 
+                                variant="contained" 
+                                color="primary" 
+                                style={
+                                    {
+                                        color:'white', 
+                                        background:'#d85232'
+                                    }
+                                }>
+                                Empieza gratis
+                                <KeyboardArrowRight />
+                            </Button>
                         </Grid>
-                    </Card>
+                    </Grid>
                 </Grid>
 
                 <style jsx>{`
                     .Portada {
                         width: 100%;
+                        height: 370px;
+                        background: #0099b0; /* Old browsers */
+                        background: linear-gradient(45deg, #0099b0, #69b893); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+                    }
+                    .puzzleRight {
+                        background-image: url("static/puzzleRight.png");
+                        background-size: 203px;
+                        background-repeat: no-repeat;
+                        opacity: 0.3;
+                        height: 345px;
+                        width: 210px;
+                        position: absolute;
+                    }
+                    .puzzleLeft {
+                        background-image: url("static/puzzleLeft.png");
+                        background-size: 203px;
+                        background-repeat: no-repeat;
+                        opacity: 0.3;
+                        height: 345px;
+                        width: 210px;
+                        position: absolute;
+                        right: 0%;
                     }
                 `}</style>
             </div>
