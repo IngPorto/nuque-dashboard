558e47a125b3398fc793523375a4f7ef219072d6
ingporto <ing.portocarrero@gmail.com>
2019-03-28 20:25:47 -0500

    [update] adding the contain of the section

--- components/SeccionQueEs/index.js
+++ components/SeccionQueEs/index.js
@@ -7,24 +7,115 @@ class main_SeccionQueEs extends React.Component{
             <div className="SeccionQueEs">
                 <div className="contenido">
                     <Grid item lg={12}>
-                        <Typography variant="body1">
-                            ¿Qué es?
-                        </Typography>
-                        <img 
-                            src="/static/logo_nuque_basic.svg"
-                            style= {{
-                                width: '50px'
-                            }}
-                        />
+                        <Grid container>
+                            <Grid item xs={9} style={{
+                                margin:'0 auto'
+                            }}>
+                                <div className="tituloSeccion">
+                                    <Typography variant="h5" style={{
+                                        width: 'fit-content',
+                                        margin: '0 auto'
+                                    }}>
+                                        ¿Qué es &nbsp;
+                                        <img 
+                                            src="/static/logo_nuque_basic.svg"
+                                            style= {{
+                                                width: '80px',
+                                                position: 'relative',
+                                                top: '7px',
+                                                right: '5px'
+                                            }}
+                                        />
+                                        ?
+                                    </Typography>
+                                </div>
+                                <div className="descripcionSeccion">
+                                    <Typography variant="body1">
+                                        Es una plataforma de programación visual para crear servicios web. Desarrollar software requiere concer uno o varios lenguajes de programación, en <span className="nuqueEnTexto">nuque</span> solo requires de lógica.
+                                    </Typography>
+                                </div>
+                            </ Grid>
+                            { /* Separador de descipción */ }
+                            <Grid item style={{
+                                margin:'0 auto'
+                            }}>
+                                <div className="seccionDescripcion">
+                                    <div className="descripcioItem">
+                                        <img src="/static/facil_icon.svg" alt="" />
+                                        <Typography variant="h5" style={{
+                                            margin: '15px 0',
+                                            fontWeight: '350'
+                                        }}>
+                                            Fácil
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            Entorno de desarrollo a partir de formas, colores y tamaños.
+                                        </Typography>
+                                    </div>
+                                    <div className="descripcioItem">
+                                        <img src="/static/universal_icon.svg" alt="" />
+                                        <Typography variant="h5" style={{
+                                            margin: '15px 0',
+                                            fontWeight: '350'
+                                        }}>
+                                            Universal
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            Integra un lenguaje de programación visual en el que hasta un niño puede crear programas.
+                                        </Typography>
+                                    </div>
+                                    <div className="descripcioItem">
+                                        <img src="/static/funcional_icon.svg" alt="" />
+                                        <Typography variant="h5" style={{
+                                            margin: '15px 0',
+                                            fontWeight: '350'
+                                        }}>
+                                            Funcional
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            Los proyectos desarrollados son 100% operativos y transparentes para las demás capas de una aplicacioón web.
+                                        </Typography>
+                                    </div>
+                                </div>
+                            </Grid>
+                        </ Grid>
                     </Grid>
                 </div>
                 <style jsx>{`
                     .SeccionQueEs {
                         width: 100%;
+                        height: 500px;
                     }
                     .contenido {
                         width: fit-content;
                         margin: 0 auto;
+                    }
+                    .SeccionQueEs *::selection {
+                        background-color: #79FFE1;
+                        color: #000;
+                    }
+                    .tituloSeccion {
+                        font-size: 1.7rem;
+                        font-weight: 450;
+                    }
+                    .descripcionSeccion {
+                        margin-top: 20px;
+                    }
+                    .nuqueEnTexto {
+                        color: #008c91;
+                        font-weight: 500;
+                    }
+                    .seccionDescripcion {
+                        margin: 40px auto;
+                    }
+                    .descripcioItem {
+                        width: 200px;
+                        text-align: center;
+                        float: left;
+                        margin: 0 15px;
+                    }
+                    .descripcioItem img{
+                        height: 100px;
                     }
                 `}</style>
             </div>
