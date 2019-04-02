0dc131eafafd879beb3147acafca9778d7a007f8
ingporto <ing.portocarrero@gmail.com>
2019-04-01 20:06:59 -0500

    [update] building the main structure of global menu

--- components/MenuGlobal/index.js
+++ components/MenuGlobal/index.js
@@ -1,11 +1,60 @@
 /**
  * Nombre del usuario , buscador de servicios
  */
+import Typography from '@material-ui/core/Typography';
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
+
 class index_MenuGlobal extends React.Component {
     render(){
         return (
             <div className="MenuGlobal">
-                MenuGlobal
+                <div className="contenedorOpcionesDeMenu">
+                    {'Opciones de menú'}
+                </div>
+                <div className="contenedorUsuarioMenu">
+                    <Typography variant="body1" style={{
+                        width:'fit-content',
+                        float: 'left',
+                        margin: '6px 10px'
+                    }}>
+                        David Portocarrero Caicedo
+                    </Typography>
+                    <img className="imgUserMenu" src="static/david_porto.jpg" alt="" />
+                    <KeyboardArrowDown style={{
+                        color: '#8f8f8f'
+                    }}/>
+                </div>
+                <div className="separadorMenuGlobal">
+                    <hr style={{
+                        opacity: '0.3',
+                        width: '100%'
+                    }}/>
+                </div>
+                <style jsx>{`
+                    .MenuGlobal {
+                        /* height: 34px; */
+                        /*float: left;*/
+                        width: 100%;
+                        margin-top: 15px;
+                    }
+                    .contenedorOpcionesDeMenu {
+                        width: fit-content;
+                        height: fit-content;
+                        float: left;
+                    }
+                    .contenedorUsuarioMenu {
+                        width: fit-content;
+                        float: right;
+                        margin-bottom: 10px;
+                    }
+                    .imgUserMenu {
+                        height: 30px;
+                        clip-path: circle(15px at center);
+                    }
+                    .separadorMenuGlobal {
+                        width: 100%;
+                    }
+                `}</style>
             </div>
         )
     }
