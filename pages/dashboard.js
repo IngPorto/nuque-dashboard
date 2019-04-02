0c966f5522fd30f4ba5025b967523c0b2735295f
ingporto <ing.portocarrero@gmail.com>
2019-04-01 20:05:22 -0500

    [update] building the html components structure of dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -9,6 +9,8 @@ import BarraLateral from "../components/BarraLateral";
 import MenuGlobal from "../components/MenuGlobal";
 import MenuDeOpcionesDeProyecto from "../components/MenuDeOpcionesDeProyecto";
 import PanelDeServicios from "../components/PanelDeServicios";
+import AreaPrincipalDashboard from "../components/AreaPrincipalDashboard";
+import Grid from '@material-ui/core/Grid';
 
  export default class extends React.Component {
     render(){
@@ -17,16 +19,64 @@ import PanelDeServicios from "../components/PanelDeServicios";
                 <div >
                     { /* Icono, botón nuevo proyecto, tus proyectos, buscador */}
                     <BarraLateral />
-                    { /* Nombre del usuario , buscador de servicios */}
-                    <MenuGlobal />
-                    { /* Descripción del proyecto, botón importar servicio */}
-                    <MenuDeOpcionesDeProyecto />
-                    { /* 1. botón nuevo servicio, crea tu primer servicio. 2. todos los servcios */}
-                    <PanelDeServicios />
+                    <AreaPrincipalDashboard>
+                        { /* Nombre del usuario , buscador de servicios */}
+                        <MenuGlobal />
+                        { /* Descripción del proyecto, botón importar servicio */}
+                        <MenuDeOpcionesDeProyecto />
+                        { /* 1. botón nuevo servicio, crea tu primer servicio. 2. todos los servcios */}
+                        <PanelDeServicios />
+                    </AreaPrincipalDashboard>
                 </div>
-                <style jsx>{`
+                <style jsx global>{`
+                    @font-face {
+                        font-family: 'Roboto-thin';
+                        src: url(static/fonts/Roboto-Thin.ttf);
+                    }
+                    @font-face {
+                        font-family: 'Roboto-light';
+                        src: url(static/fonts/Roboto-Light.ttf);
+                    }
+                    @font-face {
+                        font-family: 'Roboto-regular';
+                        src: url(static/fonts/Roboto-Regular.ttf);
+                    }
+                    @font-face {
+                        font-family: 'Roboto-medium';
+                        src: url(static/fonts/Roboto-Medium.ttf);
+                    }
+                    @font-face {
+                        font-family: 'Roboto-bold';
+                        src: url(static/fonts/Roboto-Bold.ttf);
+                    }
+                    @font-face {
+                        font-family: 'Roboto-black';
+                        src: url(static/fonts/Roboto-Black.ttf);
+                    }
+                    body {
+                        margin: 0;
+                        padding: 0;
+                    }
                     .mainContainerDashboard {
                         width: 100%;
+                    }
+                    .font-roboto-thin {
+                        font-family: 'Roboto-thin', sans-serif;
+                    }
+                    .font-roboto-light {
+                        font-family: 'Roboto-light', sans-serif;
+                    }
+                    .font-roboto-regular {
+                        font-family: 'Roboto-regular', sans-serif;
+                    }
+                    .font-roboto-medium {
+                        font-family: 'Roboto-medium', sans-serif;
+                    }
+                    .font-roboto-bold {
+                        font-family: 'Roboto-bold', sans-serif;
+                    }
+                    .font-roboto-black {
+                        font-family: 'Roboto-black', sans-serif;
                     }
                 `}</style>
             </div>
