c91693c3bbf5b385d8a6bdfee9c2e2a470ba8f5a
ingporto <ing.portocarrero@gmail.com>
2019-04-09 20:07:46 -0500

    [update] creating the visual structure from design

--- components/MenuDeOpcionesDeProyecto/index.js
+++ components/MenuDeOpcionesDeProyecto/index.js
@@ -6,35 +6,91 @@ import MoveToInbox from '@material-ui/icons/MoveToInbox';
 import Typography from '@material-ui/core/Typography';
 
 class index_MenuDeOpcionesDeProyecto extends React.Component {
-    render(){
+    render(){ 
         return(
             <div className="MenuDeOpcionesDeProyecto">
-                <div>
-                    <button className="btnEditarDescripcion"><Create /></button>
+                <div className="contenedorDescripcionProyecto">
+                    <button className="btnEditarDescripcion"><Create className="" /></button>
+                    <p className="textoDescripcionProyecto">{ 'Nueva descripción del proyecto' }</p>
+                    <button className="btnImportarServicio">
+                        <MoveToInbox className="" style={{
+                            float: 'left',
+                            margin: '3px'
+                        }}/> 
+                        <p>Importar servicio</p>
+                    </button>
                 </div>
-                { 'Nueva descripción del proyecto' }
-                <div>
-                    <button className=""><MoveToInbox /> Importar servicio</button>
+                { /* <div className="contenedorBtnImportarServicio">
+                    
+                    </div> */ }
+                <div className="contenedorTituloTusServicios">
+                    <Typography variant="body1" style={{
+                            color:'#393939',
+                            fontSize: '10pt',
+                            fontWeight: '500',
+                            float: 'left',
+                            marginLeft: '15px'
+                        }}>
+                        Tus servicios
+                    </Typography>
                 </div>
-                <Typography variant="body1" style={{
-                        color:'#393939',
-                        fontSize: '10pt',
-                        fontWeight: '500',
-                        float: 'left',
-                        marginLeft: '15px'
-                    }}>
-                    Tus servicios
-                </Typography>
                 <style jsx>{`
                     .MenuDeOpcionesDeProyecto {
                         width: 100%;
                     }
+                    .contenedorDescripcionProyecto {
+                        width: 100%;
+                        height: 70px;
+                        float: left;
+                        padding-left: 19px;
+                    }
+                    .textoDescripcionProyecto{
+                        float: left;
+                    }
+
+                    .btnImportarServicio{
+                        float: right;
+                        margin-right: 38px;
+                    }
+
+                    .contenedorBtnImportarServicio {
+                        width: 29%;
+                        float: right;
+                    }
+                    .contenedorTituloTusServicios {
+                        width: 100%;
+                        float: left;
+                    }
                     .btnEditarDescripcion {
-                        height: 30px;
-                        width: 30px;
-                        border: #cfd4d9;
-                        text-align: center;
-                        color: #2f2f2f;
+                        height: 35px;
+                        width: 36px;
+                        float: left;
+                    }
+                    .btnEditarDescripcion, .btnImportarServicio {
+                        -moz-box-shadow:inset 0px 1px 3px 0px #f0f0f0;
+                        -webkit-box-shadow:inset 0px 1px 3px 0px #f0f0f0;
+                        box-shadow:inset 0px 1px 3px 0px #f0f0f0;
+                        background-color:transparent;
+                        -moz-border-radius:5px;
+                        -webkit-border-radius:5px;
+                        border-radius:5px;
+                        border:1px solid #cfd4d9;
+                        display:inline-block;
+                        cursor:pointer;
+                        color:#2f2f2f;
+                        text-decoration:none;
+                        text-shadow:0px -1px 0px #ffffff;
+                    }
+                    .btnEditarDescripcion:hover, .btnImportarServicio:hover {
+                        background-color:transparent;
+                    }
+                    .btnEditarDescripcion:active, .btnImportarServicio:active {
+                        position:relative;
+                        top:1px;
+                    }
+                    .btnImportarServicio p {
+                        float: left;
+                        margin: 8px;
                     }
                 `}</style>
             </div>
