b53225e42321db096a2904f8cafe985bd4443e90
ingporto <ing.portocarrero@gmail.com>
2019-04-16 23:03:38 -0500

    [add] global generator file of state, it's mandatory for use Redux in Next.js

---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+/**
+ * Este archivo es necesario para que funcione REDUX en NEXT.JS.
+ * Funciona com el contenedor del estado globar, así todos los 
+ * demás páginas y otros componentes solo tienen que usar el 
+ * connect para acceder al estado o a los action para cambiar el
+ * estado. Para ello se usa el mapStateToProps y mapDispatchToProps
+ * como normalmente se usa en REDUX.
+ * ref: https://github.com/zeit/next.js/blob/master/examples/with-redux-wrapper/
+ */
+import React from 'react'
+import { Provider } from 'react-redux'
+import App, { Container } from 'next/app'
+import withRedux from 'next-redux-wrapper'
+import { initStore } from '../store'
+
+export default withRedux(initStore)(
+  class MyApp extends App {
+    static async getInitialProps ({ Component, ctx }) {
+      return {
+        pageProps: Component.getInitialProps
+          ? await Component.getInitialProps(ctx)
+          : {}
+      }
+    }
+
+    render () {
+      const { Component, pageProps, store } = this.props
+      return (
+        <Container>
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </Container>
+      )
+    }
+  }
+)
