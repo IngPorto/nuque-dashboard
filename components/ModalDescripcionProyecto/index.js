cf219b5c1a37c9a1d362de7a09972c4e9387f011
ingporto <ing.portocarrero@gmail.com>
2019-05-26 20:40:29 -0500

    [update] UI elements and logic conection for edit the description of a project

--- components/ModalDescripcionProyecto/index.js
+++ components/ModalDescripcionProyecto/index.js
@@ -1,3 +1,6 @@
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default class extends React.Component {
     constructor(props){
@@ -6,13 +9,14 @@ export default class extends React.Component {
     }
 
     componentDidMount(){
-        this.textModalDescripcionProyecto.value = this.props.initalState.proyectos[0].descripcion
+        this.textModalDescripcionProyecto.value = this.props.initalState.proyectos[this.props.initalState.proyectoActualmenteSeleccionado].descripcion
         console.log("Did mount Modal descripcion proyecto")
     }
     
     handleClickModalDescripcionProyectol = (event) =>{
         event.preventDefault();
-        this.props.addCambiarDescripcionProyecto(this.textModalDescripcionProyecto.value)
+        console.log("Proyecto ams: " + this.props.initalState.proyectoActualmenteSeleccionado)
+        this.props.addCambiarDescripcionProyecto( this.props.initalState.proyectoActualmenteSeleccionado, this.textModalDescripcionProyecto.value)
         this.props.handleClickEditarDescripcion()
         // validar la descripción
         // guardar la descipción, cerrar ventana
@@ -20,27 +24,116 @@ export default class extends React.Component {
         console.log("Click event: Editar descripcion")
     }
     
-    capturarRelModalDescripcionProyecto = (rel) =>{
-        this.textModalDescripcionProyecto = rel
+    capturarRelModalDescripcionProyecto = (ref) =>{
+        this.textModalDescripcionProyecto = ref
     }
     
     render(){
         return (
             <div className="modalDescripcionProyecto">
-                <form onSubmit={ this.handleClickModalDescripcionProyectol }>
-                    <p>Escribe una descripción para el proyecto</p>
-                    <input
-                        id="inputModalDescripcionProyecto"
-                        multiline
-                        rowsMax="4"
-                        ref={ this.capturarRelModalDescripcionProyecto }
-                        className="inputModalDescripcionProyecto"
-                    />
-                    <button className="btnAceptarModalDescripcionProyecto">
-                        Aceptar
-                    </button>
-                    <p>{ "Mensaje de error " }</p>
-                </form>
+                <div className="contenidoModalDescripcionProyecto">
+                    <buttom onClick={ this.props.cerrarModalEditarDescripcion }>
+                        <span className="botonCerrarVentana" >
+                            &times;
+                        </span>
+                    </buttom>
+                    <form onSubmit={ this.handleClickModalDescripcionProyectol }>
+                        <Typography variant="body1" style={{
+                                color:'#393939',
+                                fontSize: '12pt',
+                                fontWeight: '400',
+                                marginBottom: '20px',
+                            }}>
+                            Escribe una descripción para el proyecto
+                        </Typography>
+                        <textarea 
+                            id="inputModalDescripcionProyecto"
+                            type="text"
+                            cols="53"
+                            rows="5"
+                            ref={ this.capturarRelModalDescripcionProyecto }
+                            className="inputModalDescripcionProyecto"
+                        />
+                        <Button 
+                            className="btnAceptarModalDescripcionProyecto" 
+                            variant="contained" 
+                            color="primary" 
+                            onClick={ this.handleClickModalDescripcionProyectol }
+                            style={
+                                {
+                                    color:'white', 
+                                    background:'#2196f3',
+                                    borderRadius: '0px',
+                                    padding: '7px 30px',
+                                    marginRight: '8px',
+                                    float: 'right'
+                                }
+                            }>
+                            Aceptar
+                        </Button>
+                        <hr style={{
+                            opacity: '0.3',
+                            marginTop: '45px',
+                            marginBottom: '0'
+                        }}/>
+                        <p>{ /*"Mensaje de error "*/ }</p>
+                    </form>
+                </div>
+                <div 
+                    className="backgroundFiller"
+                    onClick={ this.props.cerrarModalEditarDescripcion }
+                >
+                </div>
+                <style jsx>{`
+                    .contenidoModalDescripcionProyecto {
+                        position: fixed;
+                        width: 400px;
+                        height: 200px;
+                        top: 30%;
+                        left: calc(50vw - (200px + 15px));
+                        border-style: solid;
+                        border-width: 1px;
+                        border-color: #cccccc;
+                        border-radius: 0px;
+                        padding: 15px;
+                        background-color: white;
+                        z-index: 9999;
+                        
+                        -webkit-box-shadow: 0px 2px 30px -15px rgba(0,0,0,0.41);
+                        -moz-box-shadow: 0px 2px 30px -15px rgba(0,0,0,0.41);
+                        box-shadow: 0px 2px 30px -15px rgba(0,0,0,0.41);
+                    }
+
+                    .botonCerrarVentana {
+                        cursor: pointer;
+                        position: absolute;
+                        top: 0;
+                        right: 0;
+                        padding: .75rem 1.25rem;
+                        color: inherit;
+                        float: right;
+                        font-size: 1.5rem;
+                        font-weight: 700;
+                        line-height: 1;
+                        color: #000;
+                        text-shadow: 0 1px 0 #fff;
+                        opacity: .5;
+                    }
+                    .backgroundFiller {
+                        position: fixed;
+                        top: 0;
+                        left: 0;
+                        width: 100%;
+                        height: 100%;
+                    }
+                    .btnAceptarModalDescripcionProyecto {
+                        float: right;
+                    }
+                    #inputModalDescripcionProyecto {
+                        resize: none;
+                        font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+                    }
+                `}</style>
             </div>
         )
     }
