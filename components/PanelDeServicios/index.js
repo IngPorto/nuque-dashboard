ffd95a7a588a6b4d8eb0210f267c9674f7c6a638
ingporto <ing.portocarrero@gmail.com>
2019-05-26 20:45:40 -0500

    [update] unnecesary imports removed, create a component for display each service card

--- components/PanelDeServicios/index.js
+++ components/PanelDeServicios/index.js
@@ -2,42 +2,47 @@
  * 1. botón nuevo servicio, crea tu primer servicio. 2. todos los servcios
  */
 import Add from '@material-ui/icons/Add';
-import Widgets from '@material-ui/icons/Widgets';
-import ToggleOn from '@material-ui/icons/ToggleOn';
-import ToggleOff from '@material-ui/icons/ToggleOff';
-import BubbleChart from '@material-ui/icons/BubbleChart';
-import MoreVert from '@material-ui/icons/MoreVert';
-import FileCopy from '@material-ui/icons/FileCopy';
-import LowPriority  from '@material-ui/icons/LowPriority';
-import Create from '@material-ui/icons/Create';
-import Delete from '@material-ui/icons/Delete';
+import CardServicio from '../CardServicio';
+import ModalNuevoElemento from '../ModalNuevoElemento';
 
 class index_PanelDeServicios extends React.Component {
-
     constructor(props){
         super(props)
-        this.props.addCambiarProyectoActualmenteSeleccionado(0)
+        this.state = {
+            modalNuevoServicio: false
+        }
     }
-
+    
     componentWillMount(){
     }
 
-    handleClickSubMenuPerfilServicio = () =>{
-        this.props.addCambiarVisibilidadSMPS(!this.props.initalState.visibilidadSubMenuPerfilServicio)
-        //console.log (this.props)
+    handleModalNuevoServicio = () => {
+        this.setState({
+            ...this.state,
+            modalNuevoServicio: !this.state.modalNuevoServicio
+        })
+    }
+
+    crearNuevoServicio = (nombre, descripcion) => {
+        this.props.addCrearNuevoServicio( nombre, descripcion, this.props.initalState.proyectoActualmenteSeleccionado )
+        this.handleModalNuevoServicio()
     }
 
     render(){
         // Como el primer render trae datos vacíos en el initial state, solo uso el initial state cuando el tipo de dato sea un numero y no vacío
         const servicios = typeof this.props.initalState.proyectoActualmenteSeleccionado == 'number' && this.props.initalState.proyectos[this.props.initalState.proyectoActualmenteSeleccionado].servicios 
         //console.log("Proyecto seleccionado: " + this.props.initalState.proyectoActualmenteSeleccionado)
-        //console.log(servicios)
+        //const servicios = null;
+        console.log(servicios)
 
         return(
             <div className="PanelDeServicios">
                 <div className="contenedorCentralPanelDeServicios">
                     <div className="contenedorBtnNuevoServicio">
-                        <button className="btnNuevoServicio">
+                        <button 
+                            className="btnNuevoServicio"
+                            onClick= { this.handleModalNuevoServicio }
+                        >
                             <Add style={{
                             }}/>
                         </button>
@@ -49,98 +54,25 @@ class index_PanelDeServicios extends React.Component {
                             servicios &&
                             servicios.map(servicio =>{
                                 return(
-                                    <div className="perfilDeServicio">
-                                        <div className="headerPerfilDeServicio">
-                                            <Widgets style={{
-                                                color: '#2f2f2f',
-                                                float: 'left'
-                                            }} />
-                                            <p className="resetText tituloPerfilServicio font-roboto-regular">{ this.props.initalState.servicios[servicio].nombre }</p>
-                                            <MoreVert 
-                                                style={{
-                                                    float: 'right',
-                                                    color: '#2f2f2f',
-                                                    width: '23px',
-                                                    cursor: 'pointer' 
-                                                }}
-                                                onClick= { this.handleClickSubMenuPerfilServicio }
-                                            />
-                                            <p className="textoEnLinea resetText textoPerfilServicio font-roboto-regular">{this.props.initalState.servicios[servicio].estado}</p>
-                                            <div className={"Toggle "+ this.props.initalState.servicios[servicio].toggle}>
-                                                <ToggleOn style={{
-                                                    float: 'right',
-                                                    height: '40px',
-                                                    width: '40px',
-                                                    position: 'relative',
-                                                    top: '-8px',
-                                                    cursor: 'pointer' }}
-                                                />
-                                            </div>                                            
-                                        </div>
-                                        <div className="bodyPerfilDeServicio">
-                                            <p className="resetText textoPerfilServicio font-roboto-regular">Acceso:</p>
-                                            <p className="resetText textoPerfilServicio font-roboto-regular">Código:</p>
-                                            <p className="resetText textoPerfilServicio font-roboto-regular">Protocolo:</p>
-                                            <p className="resetText textoPerfilServicio font-roboto-regular">Formato:</p>
-                                        </div>
-                                        <div className="footerPerfilDeServicio">
-                                            <p className="resetText textoPerfilServicio font-roboto-regular">Creado el {this.props.initalState.servicios[servicio].fechaCreacion}</p>
-                                        </div>
-                                        { 
-                                            this.props.initalState.visibilidadSubMenuPerfilServicio && 
-                                        
-                                        <div className="SubMenuPerfilServicio">
-                                            <div className="marcoSubMenu">
-                                                <div className="SubMenuPerfilServicioOpcion">
-                                                    <FileCopy style={{
-                                                        float: 'left',
-                                                        width: '15px',
-                                                        marginLeft: '12px'
-                                                    }}/>
-                                                    <p className="textoOpcionServicio resetText font-roboto-regular">
-                                                        Duplicar
-                                                    </p>
-                                                </div>
-                                                <div className="SubMenuPerfilServicioOpcion">
-                                                    <LowPriority style={{
-                                                        float: 'left',
-                                                        width: '15px',
-                                                        marginLeft: '12px'
-                                                    }}/>
-                                                    <p className="textoOpcionServicio resetText font-roboto-regular">
-                                                        Mover de proyecto
-                                                    </p>
-                                                </div>
-                                                <div className="SubMenuPerfilServicioOpcion">
-                                                    <Create style={{
-                                                        float: 'left',
-                                                        width: '15px',
-                                                        marginLeft: '12px'
-                                                    }}/>
-                                                    <p className="textoOpcionServicio resetText font-roboto-regular">
-                                                        Modificar
-                                                    </p>
-                                                </div>
-                                                <div className="SubMenuPerfilServicioOpcion">
-                                                    <Delete style={{
-                                                        float: 'left',
-                                                        width: '15px',
-                                                        marginLeft: '12px'
-                                                    }}/>
-                                                    <p className="textoOpcionServicio resetText font-roboto-regular">
-                                                        Eliminar
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        }
-                                    </div>
+                                    <CardServicio 
+                                        {...this.props}  
+                                        servicio = {servicio}
+                                    />
                                 )
                             })
                         }
-                        <div className="perfilDeServicio"></div>
                     </div>
                 </div>
+                {
+                    this.state.modalNuevoServicio &&
+                    <ModalNuevoElemento 
+                        { ...this.props }
+                        handleClickAceptar = { this.crearNuevoServicio }
+                        cerrarModal = { this.handleModalNuevoServicio }
+                        textoLabel_1 = "Nombre del servicio"
+                        textoLabel_2 = "Descripción del servicio"
+                    />
+                }
                 <style jsx>{`
                     .PanelDeServicios {
                         float: left;
@@ -167,23 +99,7 @@ class index_PanelDeServicios extends React.Component {
                         float: initial;
                         cursor: pointer;
                     }
-                    .resetText {
-                        margin: 0;
-                        padding: 0;
-                    }
-                    .textoNuevoServicio {
-                        width: fit-content;
-                        margin: 5px auto 0 auto;
-                        padding: 0;
-                    }
-                    .perfilDeServicio {
-                        width: 341px;
-                        height: 116px;
-                        margin-right: 26px;
-                        margin-bottom: 33px;
-                        padding: 15px;
-                    }
-                    .btnNuevoServicio, .perfilDeServicio{
+                    .btnNuevoServicio {
                         -moz-box-shadow:inset 0px 1px 3px 0px #f0f0f0;
                         -webkit-box-shadow:inset 0px 1px 3px 0px #f0f0f0;
                         box-shadow:inset 0px 1px 3px 0px #f0f0f0;
@@ -203,89 +119,11 @@ class index_PanelDeServicios extends React.Component {
                         position:relative;
                         top:1px;
                     }
-                    
-                    .headerPerfilDeServicio {
-                        float: left;
-                        width: 100%;
-                    }
-                    .bodyPerfilDeServicio {
-                        width: 100%;
-                        height: fit-content;
-                        float: left;
-                    }
-                    .footerPerfilDeServicio {
-                        width: 100%;
-                        float: right;
-                        text-align: right;
-                    }
-                    .tituloPerfilServicio {
-                        color: #005da7;
-                        font-size: 0.879rem;
-                        cursor: pointer;
-                        float: left;
-                        width: 200px;
-                        margin: 3px 0 0 5px;
-                    }
-                    .tituloPerfilServicio:active {
-                        color: #082c49;
-                        outline: -webkit-focus-ring-color auto 5px;
-                    }
-                    .textoPerfilServicio {
-                        font-size: 0.76rem;
-                        color: #525f68;
-                    }
-                    .Toggle {
-                        float: left;
+                    .textoNuevoServicio {
                         width: fit-content;
+                        margin: 5px auto 0 auto;
+                        padding: 0;
                     }
-                    .ToggleOn {
-                        color: green;
-                    }
-                    .ToggleOff {
-                        color: #b4b4b4;
-                        transform: rotate(180deg);
-                        height: 23px;
-                    }
-                    .textoEnLinea {
-                        float: right;
-                        width: fit-content;
-                        margin-top: 3px;
-                    }
-
-                    .SubMenuPerfilServicio {
-                        width: 147px;
-                        height: 121px;
-                        -webkit-box-shadow: -2px 3px 10px -6px rgba(0,0,0,0.75);
-                        -moz-box-shadow: -2px 3px 10px -6px rgba(0,0,0,0.75);
-                        box-shadow: -2px 3px 10px -6px rgba(0,0,0,0.75);
-                        background-color: white;
-                        position: relative;
-                        left: 200px;
-                        top: 30px;
-                    }
-                    .marcoSubMenu {
-                        position: absolute;
-                        overflow: hidden;
-                        width: 100%;
-                        padding-top: 10px;
-                    }
-                    .SubMenuPerfilServicioOpcion {
-                        height: fit-content;
-                        width: 100%;
-                        width: 100%;
-                        height: 27px;
-                    }
-                    .SubMenuPerfilServicioOpcion:hover{
-                        background-color: #005da7;
-                        color: white;
-                        cursor: pointer;
-                    }
-                    .textoOpcionServicio {
-                        float: left;
-                        margin: 5px;
-                        font-size: 0.8rem;
-                    }
-                    
                 `}</style>
             </div>
         )
