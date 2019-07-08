94f2b4d6ed280067e997f8f0fa30f6405e80ef18
ingporto <ing.portocarrero@gmail.com>
2019-07-08 18:42:15 -0500

    [update] blocks configurations

--- components/Playground/index.js
+++ components/Playground/index.js
@@ -40,6 +40,275 @@ class Playground extends React.Component {
         */
         //console.log(workspace)
         //console.log(Blockly)
+
+        //:::::::::::::::::Construyo mis propios bloques:::::::::::::::::::
+        //______Web_______
+        /**
+         * Console.log
+         */
+        Blockly.Blocks['console_log'] = {
+            init: function() {
+                this.appendValueInput("NAME")
+                    .setCheck(null)
+                    .appendField("Mostrar en consola");
+                this.setPreviousStatement(true, null);
+                this.setNextStatement(true, null);
+                this.setColour(160);
+                this.setTooltip("Permite imprimir por consola del servidor.");
+                this.setHelpUrl("");
+            }
+        };
+        Blockly.JavaScript['console_log'] = function(block) {
+            var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+            // TODO: Assemble JavaScript into code variable.
+            var code = 'console.log(' + value_name + ');';
+            return code;
+        };
+
+        /**
+         * Params request
+         */
+        Blockly.Blocks['parametro_get'] = {
+            init: function() {
+              this.appendDummyInput()
+                  .appendField("Parámetro de entrada")
+                  .appendField(new Blockly.FieldTextInput("nombre"), "NAME");
+              this.setOutput(true, null);
+              this.setColour(300);
+           this.setTooltip("Acceso a el parámetro de entrada disponible en la URL y retorna el valor asignado");
+           this.setHelpUrl("https://support.google.com/google-ads/answer/6277564?hl=es-419");
+            }
+        };
+        Blockly.JavaScript['parametro_get'] = function(block) {
+            var text_name = block.getFieldValue('NAME');
+            // TODO: Assemble JavaScript into code variable.
+            var code = 'req.query.'+text_name;
+            // TODO: Change ORDER_NONE to the correct strength.
+            return [code, Blockly.JavaScript.ORDER_ATOMIC];
+        };
+        /**
+         * IP request
+         */
+        Blockly.Blocks['request_ip'] = {
+            init: function() {
+              this.appendDummyInput()
+                  .appendField("IP del cliente");
+              this.setOutput(true, null);
+              this.setColour(300);
+           this.setTooltip("Dirección IP de la solicitud");
+           this.setHelpUrl("https://expressjs.com/es/api.html#req.ip");
+            }
+        };
+        Blockly.JavaScript['request_ip'] = function(block) {
+            // TODO: Assemble JavaScript into code variable.
+            var code = 'req.ip;\n';
+            // TODO: Change ORDER_NONE to the correct strength.
+            return [code, Blockly.JavaScript.ORDER_NONE];
+          };
+        /**
+         * Content-Type request acepted
+         */
+        Blockly.Blocks['request_content_type'] = {
+            init: function() {
+              this.appendDummyInput()
+                  .appendField("Tipo de parámetro de entrada  aceptado")
+                  .appendField(new Blockly.FieldDropdown([["html","html"], ["json","json"], ["xml","xml"], ["text/plain","text/plain"], ["text/csv","text/csv"]]), "NAME");
+              this.setPreviousStatement(true, null);
+              this.setNextStatement(true, null);
+              this.setColour(300);
+           this.setTooltip("Verifica el tipo de contenido proveniente en la consulta HTTP.");
+           this.setHelpUrl("https://developer.mozilla.org/es/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Lista_completa_de_tipos_MIME");
+            }
+          };
+        Blockly.JavaScript['request_content_type'] = function(block) {
+            var dropdown_name = block.getFieldValue('NAME');
+            // TODO: Assemble JavaScript into code variable.
+            var code = 'req.accepts(\''+ dropdown_name +'\');\n';
+            return code;
+          };
+        /**
+         * Send response
+         */
+        Blockly.Blocks['response_send'] = {
+            init: function() {
+              this.appendValueInput("NAME")
+                  .setCheck(null)
+                  .appendField("Responder con");
+              this.setPreviousStatement(true, null);
+              this.setNextStatement(true, null);
+              this.setColour(300);
+           this.setTooltip("Enviar una respuesta HTTP");
+           this.setHelpUrl("https://expressjs.com/es/api.html#res.send");
+            }
+          };
+        Blockly.JavaScript['response_send'] = function(block) {
+            var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+            // TODO: Assemble JavaScript into code variable.
+            var code = 'res.send('+ value_name +');\n';
+            return code;
+          };
+        /**
+         * Redirect response
+         */
+        Blockly.Blocks['response_redirect'] = {
+            init: function() {
+              this.appendValueInput("NAME")
+                  .setCheck(null)
+                  .appendField("Ir a la URL");
+              this.setPreviousStatement(true, null);
+              this.setColour(300);
+           this.setTooltip("Redireccionar a una URL específica con un status \"302 Found\" por defecto");
+           this.setHelpUrl("https://expressjs.com/es/api.html#res.redirect");
+            }
+          };
+        Blockly.JavaScript['response_redirect'] = function(block) {
+            var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+            // TODO: Assemble JavaScript into code variable.
+            var code = 'res.redirect('+ value_name +');\n';
+            return code;
+          };
+        /**
+         * Status response
+         */
+        Blockly.Blocks['response_status'] = {
+            init: function() {
+              this.appendDummyInput()
+                  .appendField("Estado de la petición")
+                  .appendField(new Blockly.FieldDropdown([["200","200"], ["302","302"], ["400","400"], ["404","404"]]), "NAME");
+              this.setPreviousStatement(true, null);
+              this.setNextStatement(true, null);
+              this.setColour(300);
+           this.setTooltip("Asignar el estado HTTP de la petición");
+           this.setHelpUrl("https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml");
+            }
+        };
+        Blockly.JavaScript['response_status'] = function(block) {
+            var dropdown_name = block.getFieldValue('NAME');
+            // TODO: Assemble JavaScript into code variable.
+            var code = 'res.status('+ dropdown_name +');\n';
+            return code;
+        };
+        /**
+         * Content-Type response
+         */
+        Blockly.Blocks['response_content_type'] = {
+            init: function() {
+              this.appendDummyInput()
+                  .appendField("Tipo de respuesta")
+                  .appendField(new Blockly.FieldDropdown([["html","html"], ["json","json"], ["xml","xml"], ["text/plain","text/plain"], ["text/csv","text/csv"]]), "NAME");
+              this.setPreviousStatement(true, null);
+              this.setNextStatement(true, null);
+              this.setColour(300);
+           this.setTooltip("Asignar tipo de contenido en la cabecera HTTP de la respuesta.");
+           this.setHelpUrl("https://developer.mozilla.org/es/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Lista_completa_de_tipos_MIME");
+            }
+          };
+        Blockly.JavaScript['response_content_type'] = function(block) {
+            var dropdown_name = block.getFieldValue('NAME');
+            // TODO: Assemble JavaScript into code variable.
+            var code = 'res.type(\''+ dropdown_name +'\');\n';
+            return code;
+          };
+        
+        // ___________Objects____________
+        /**
+         * Create object
+         */
+        Blockly.Blocks['object_create'] = {
+            init: function() {
+              this.appendDummyInput()
+                  .appendField("un nuevo objeto vacío");
+              this.setOutput(true, null); //"Object");
+              this.setColour(300);
+           this.setTooltip("Un objeto es una colección de propiedades, y una propiedad es una asociación entre un nombre (o clave) y un valor");
+           this.setHelpUrl("https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Trabajando_con_objectos");
+            }
+          };
+        Blockly.JavaScript['object_create'] = function(block) {
+            // TODO: Assemble JavaScript into code variable.
+            var code = '{}';
+            // TODO: Change ORDER_NONE to the correct strength.
+            return [code, Blockly.JavaScript.ORDER_ATOMIC];
+          };
+        /**
+         * Object Length
+         */
+        Blockly.Blocks['object_length'] = {
+            init: function() {
+              this.appendValueInput("NAME")
+                  .setCheck("Object")
+                  .appendField("tamaño del objeto");
+              this.setOutput(true, "Number");
+              this.setColour(300);
+           this.setTooltip("Entrega la cantidad de hijos directos tiene el objeto");
+            }
+          };
+          Blockly.JavaScript['object_length'] = function(block) {
+            var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+            // TODO: Assemble JavaScript into code variable.
+            var code = 'Object.keys('+ value_name +').length';
+            // TODO: Change ORDER_NONE to the correct strength.
+            return [code, Blockly.JavaScript.ORDER_NONE];
+          };
+        /**
+         * Set value y key
+         */
+        Blockly.Blocks['object_key_value'] = {
+            init: function() {
+              this.appendDummyInput()
+                  .appendField("Asignar al objeto")
+                  .appendField(new Blockly.FieldVariable("mi_objeto"), "OBJETO");
+              this.appendValueInput("CLAVE")
+                  .setCheck(["String", "Number"])
+                  .appendField("la clave");
+              this.appendValueInput("VALOR")
+                  .setCheck(null)
+                  .appendField("y el valor");
+              this.setInputsInline(true);
+              this.setPreviousStatement(true, null);
+              this.setNextStatement(true, null);
+              this.setColour(300);
+           this.setTooltip("Asignar un nuevo elemento al objeto");
+           this.setHelpUrl("https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Trabajando_con_objectos");
+            }
+          };
+        Blockly.JavaScript['object_key_value'] = function(block) {
+            var variable_objeto = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('OBJETO'), Blockly.Variables.NAME_TYPE);
+            var value_clave = Blockly.JavaScript.valueToCode(block, 'CLAVE', Blockly.JavaScript.ORDER_ATOMIC);
+            var value_valor = Blockly.JavaScript.valueToCode(block, 'VALOR', Blockly.JavaScript.ORDER_ATOMIC);
+            // TODO: Assemble JavaScript into code variable.
+            //value_clave = value_clave.replace("'","");
+
+            var code = variable_objeto + '[' + value_clave + '] = ' + value_valor +';\n';
+            return code;
+          };
+        /**
+         * Get value from object
+         */
+        Blockly.Blocks['object_get_value'] = {
+            init: function() {
+              this.appendDummyInput()
+                  .appendField("Obtener del objeto ")
+                  .appendField(new Blockly.FieldVariable("mi_objeto"), "OBJETO");
+              this.appendValueInput("CLAVE")
+                  .setCheck(["String", "Number"])
+                  .appendField("el valor con la clave");
+              this.setInputsInline(true);
+              this.setPreviousStatement(true, null);
+              this.setNextStatement(true, null);
+              this.setColour(300);
+           this.setTooltip("Obtener el valor con una clave de un objeto");
+           this.setHelpUrl("https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Trabajando_con_objectos");
+            }
+          };
+        Blockly.JavaScript['object_get_value'] = function(block) {
+            var variable_objeto = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('OBJETO'), Blockly.Variables.NAME_TYPE);
+            var value_clave = Blockly.JavaScript.valueToCode(block, 'CLAVE', Blockly.JavaScript.ORDER_ATOMIC);
+            // TODO: Assemble JavaScript into code variable.
+            var code = variable_objeto + '.'+ value_clave +';\n';
+            return code;
+          };
+
     }
 
     handleMigaDePanRegresarAPanelDeServicios = () => {
@@ -81,6 +350,10 @@ class Playground extends React.Component {
                             }
                         </Typography>
                     </div>
+                    {
+                        this.props.visibilidadGifCarga &&
+                        <img className="gif_carga" src="/static/micro_load.gif" />
+                    }
                     <button 
                         className="btnGuardarWorkspace"
                         onClick={ this.props.handleGuardarWorkspace }
@@ -103,7 +376,7 @@ class Playground extends React.Component {
                 </div>
 
                 <div>
-                    <div ref="blocklyDiv" id="blocklyDiv" style={{ height: 'calc(100vh - 125px)', width: '100vw - 175px)'}}></div>
+                    <div ref="blocklyDiv" id="blocklyDiv" style={{ height: 'calc(100vh - 154px)', width: '100vw - 175px)'}}></div>
                 </div>
 
                 <xml id="toolbox" style={{display: 'none'}}>
@@ -324,6 +597,12 @@ class Playground extends React.Component {
                         </value>
                     </block>
                     </category>
+                    <category name="Objetos" colour="300">
+                        <block type="object_create"></block>
+                        <block type="object_key_value"></block>
+                        <block type="object_get_value"></block>
+                        <block type="object_length"></block>
+                    </category>
                     <category name="Listas" colour="%{BKY_LISTS_HUE}">
                     <block type="lists_create_with">
                         <mutation items="0"></mutation>
@@ -378,6 +657,16 @@ class Playground extends React.Component {
                     <sep></sep>
                     <category name="Variables" colour="%{BKY_VARIABLES_HUE}" custom="VARIABLE"></category>
                     <category name="Funciones" colour="%{BKY_PROCEDURES_HUE}" custom="PROCEDURE"></category>
+                    <sep></sep>
+                    <category name="Web" colour="300">
+                        <block type="parametro_get"></block>
+                        <block type="request_ip"></block>
+                        <block type="request_content_type"></block>
+                        <block type="response_send"></block>
+                        <block type="response_redirect"></block>
+                        <block type="response_status"></block>
+                        <block type="response_content_type"></block>
+                    </category>
                 </xml>
 
                 <style jsx>{`
@@ -405,6 +694,13 @@ class Playground extends React.Component {
                     .btnGuardarWorkspace:active {
                         position:relative;
                         top:1px;
+                    }
+                    .gif_carga {
+                        height: 47px;
+                        position: absolute;
+                        top: -9px;
+                        right: 102px;
+                        opacity: 0.5;
                     }
                 `}</style>
             </div>
