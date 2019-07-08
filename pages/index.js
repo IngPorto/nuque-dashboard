0b28271f2b70a26ec32084ae99e71bfe6a78a424
ingporto <ing.portocarrero@gmail.com>
2019-07-08 17:58:24 -0500

    [update] headers for Blockly global variable

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,21 @@ import FooterIndex from '../components/FooterIndex'
 
 // Material-ui components
 import Grid from '@material-ui/core/Grid';
+import Head from 'next/head'
 
 export default class extends React.Component {
     render(){
         return <div className="mainContainerIndex">
+            <Head>
+                <title>Nuque</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <script src="/static/blockly/blockly_compressed.js"></script>
+                <script src="/static/blockly/blocks_compressed.js"></script>
+                <script src="/static/blockly/msg/js/es.js"></script> 
+                <script src="/static/blockly/javascript_compressed.js"></script>
+                <link rel="stylesheet" href="/static/css/animate.min.css" />
+            </Head>
+
             <Grid container spacing={0} >
                 <NavegadorGlobal />
                 <Portada />
